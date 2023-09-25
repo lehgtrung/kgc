ae0f8ed92c1df12e9b51983d218ff9b1f1595d4f
Trung Le <le.hg.trung@gmail.com>
2023-09-25 00:29:30 -0600

    update reasoning

--- reasoning/matrix_reasoning.py
+++ reasoning/matrix_reasoning.py
@@ -54,13 +54,13 @@ def encode_rules(rule_path, max_rank=4):
         rule_body = [normalize_relation(e) for e in line[1:-2]]
         if rule_head not in rules:
             rules[rule_head] = []
-        # if len(rules[rule_head]) < max_rank:
-        #     rules[rule_head].append(
-        #         {
-        #             'conf': rule_conf,
-        #             'body': rule_body
-        #         }
-        #     )
+        if len(rules[rule_head]) < max_rank:
+            rules[rule_head].append(
+                {
+                    'conf': rule_conf,
+                    'body': rule_body
+                }
+            )
     return rules
 
 
