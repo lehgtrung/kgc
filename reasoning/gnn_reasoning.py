2dbcc8ff67d6eaefa5bb727ffe20ed58ee47aaf2
Trung Le <le.hg.trung@gmail.com>
2023-10-02 11:54:25 -0600

    test gnn

---
+++ reasoning/gnn_reasoning.py
@@ -0,0 +1,49 @@
+
+import torch
+import torch_geometric.transforms as T
+from torch_geometric.datasets import WordNet18RR
+from torch_geometric.nn import SAGEConv, to_hetero
+from reasoning_utils import load_data_encoded
+from torch_geometric.data import HeteroData
+import pandas as pd
+
+
+def import_dataset_pyg(df: pd.DataFrame, _list_ents: list[str], _list_rels: list[str]):
+    _htr_data = HeteroData()
+    _htr_data['entity'].x = torch.rand((len(_list_ents), 32))
+    for rel in _list_rels:
+        df_query = df.query(f'relation == "{rel}"')
+        if not rel.startswith('inv'):
+            _htr_data['entity', rel, 'entity'].edge_index = torch.tensor([df_query['head_idx'].tolist(),
+                                                                         df_query['tail_idx'].tolist()],
+                                                                        dtype=torch.int32)
+        else:
+            _htr_data['entity', rel, 'entity'].edge_index = torch.tensor([df_query['tail_idx'].tolist(),
+                                                                         df_query['head_idx'].tolist()],
+                                                                        dtype=torch.int32)
+    return _htr_data
+
+
+class GNN(torch.nn.Module):
+    def __init__(self, hidden_channels, out_channels):
+        super().__init__()
+        self.conv1 = SAGEConv((-1, -1), hidden_channels)
+        self.conv2 = SAGEConv((-1, -1), out_channels)
+
+    def forward(self, x, edge_index):
+        x = self.conv1(x, edge_index).relu()
+        x = self.conv2(x, edge_index)
+        return torch.sigmoid(x)
+
+
+if __name__ == '__main__':
+    df_train, list_ents, list_rels = load_data_encoded('../WN18RR/test.txt')
+    htr_data = import_dataset_pyg(df_train, list_ents, list_rels).to('cuda')
+
+    model = GNN(hidden_channels=64, out_channels=1)
+    model = to_hetero(model, htr_data.metadata(), aggr='sum').to('cuda')
+
+    with torch.no_grad():  # Initialize lazy modules.
+        out = model(htr_data.x_dict, htr_data.edge_index_dict)
+        print(out)
+
--- reasoning/reasoning_utils.py
+++ reasoning/reasoning_utils.py
@@ -21,3 +21,17 @@ def load_data_raw(path):
     data['inv_relation'] = data['relation'].apply(lambda x: normalize_relation('!' + x))
     return data
 
+
+def load_data_encoded(path):
+    data = pd.read_csv(path, sep='\t', dtype=str, header=None)
+    data.columns = ['head', 'relation', 'tail']
+    data['head'] = data['head'].apply(lambda x: 'e' + str(x))
+    data['tail'] = data['tail'].apply(lambda x: 'e' + str(x))
+    data['relation'] = data['relation'].apply(lambda x: normalize_relation(x))
+    data['inv_relation'] = data['relation'].apply(lambda x: normalize_relation('!' + x))
+    list_rels = list(set(data['relation'].tolist() + data['inv_relation'].tolist()))
+    list_ents = list(set(data['head'].tolist() + data['tail'].tolist()))
+    data['head_idx'] = data['head'].apply(lambda x: list_ents.index(x))
+    data['tail_idx'] = data['tail'].apply(lambda x: list_ents.index(x))
+    return data, list_ents, list_rels
+
