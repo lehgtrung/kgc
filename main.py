ea6bf7f0462a4adb6dae30ef78e7c1e9aa7191fe
Trung Le <le.hg.trung@gmail.com>
2023-10-17 22:24:16 -0600

    move data importing and path finding to neo4j

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from py2neo import Node, Relationship
 import pandas as pd
 from tqdm import tqdm
 import argparse
+from neo4j import GraphDatabase
 import random
 
 
@@ -39,47 +40,19 @@ def load_data(path):
     return data
 
 
-def add_entities(graph, df, dataset_name):
-    tx = graph.begin()
-    # Find all entities
-    head_entities = df['head'].unique().tolist()
-    tail_entities = df['tail'].unique().tolist()
-    all_entities = list(set(head_entities + tail_entities))
-
-    print('Creating entities ...')
-    for entity in tqdm(all_entities):
-        node = Node('Entity', name=entity, dtype='train', dt_name=dataset_name)
-        tx.create(node)
-    graph.commit(tx)
-
-
-def add_relations(graph, df, dataset_name):
-    tx = graph.begin()
-    print('Creating relations ...')
+def import_records(driver, df, dataset_name):
+    df['head'] = df['head'].apply(lambda x: 'TEST_' + x)
+    df['tail'] = df['tail'].apply(lambda x: 'TEST_' + x)
+    print('Import records ...')
     for i, row in tqdm(df.iterrows(), total=len(df)):
         head, relation, inv_relation, tail = row['head'], row['relation'], \
                                              row['inv_relation'], row['tail']
-        head_entity = graph.nodes.match('Entity', name=head,
-                                        dtype='train', dt_name=dataset_name).first()
-        tail_entity = graph.nodes.match('Entity', name=tail,
-                                        dtype='train', dt_name=dataset_name).first()
-
-        relationship = Relationship(head_entity, relation, tail_entity)
-        inv_relationship = Relationship(tail_entity, inv_relation, head_entity)
-        tx.create(relationship)
-        tx.create(inv_relationship)
-    graph.commit(tx)
-
-
-def sample_training_data(in_path, out_path, portion=0.2):
-    with open(in_path, 'r') as f:
-        data = f.readlines()
-        data = [line.strip() for line in data]
-        print('Data length: ', len(data))
-        data = random.sample(data, int(len(data) * portion))
-        print('Sample length: ', len(data))
-    with open(out_path, 'w') as f:
-        f.writelines([line + '\n' for line in data])
+        driver.execute_query(
+            f'MERGE (head:Entity {{name: "{head}", dt_name: "{dataset_name}"}}) '
+            f'MERGE (tail:Entity {{name: "{tail}", dt_name: "{dataset_name}"}}) '
+            f'MERGE (head)-[:{relation}]->(tail) '
+            f'MERGE (tail)-[:{inv_relation}]->(head)',
+        )
 
 
 def split_list_into_n_sublists(in_path, out_path, n_parts=6):
@@ -107,7 +80,10 @@ if __name__ == '__main__':
     # MATCH (n)
     # DETACH DELETE n
     # http://localhost:7474/browser/
-    graph = Graph('bolt://localhost:7687')
+
+    URI = "neo4j://localhost:7687"
+    global_driver = GraphDatabase.driver(URI)
+
     parser = argparse.ArgumentParser()
     parser.add_argument("--dataset", help="Name of dataset", required=True)
     args = parser.parse_args()
@@ -116,13 +92,9 @@ if __name__ == '__main__':
         raise ValueError('Wrong dataset name!!!')
     dataset = args.dataset
 
-    # sample_training_data(f'{dataset}/train.txt',
-    #                      f'{dataset}/train_sampled.txt',
-    #                      0.1)
-
-    # train_data = load_data(f'{dataset}/train.txt')
-    # add_entities(graph, train_data, dataset)
-    # add_relations(graph, train_data, dataset)
+    train_data = load_data(f'WN18RR/train.txt')
+    import_records(global_driver, train_data, dataset)
+    global_driver.close()
 
     # split_list_into_n_sublists(f'{dataset}/train.txt',
     #                            dataset + '/splits/part_{i}.txt',
--- path_finding.py
+++ path_finding.py
@@ -1,64 +1,46 @@
 import argparse
 from py2neo import Graph
-from py2neo import Node, Relationship
 import pandas as pd
-from main import load_dict, load_data
-from collections import defaultdict, Counter
+from main import load_data
+from collections import Counter
 from tqdm import tqdm
-import json
-import re
+from neo4j import GraphDatabase
+import time
 
 
-def extract_record_triplets(record):
-    record = str(record).replace(' ', '')
-    pattern = r'\((.*?)\)-\[\:(.*?)\{\}\]->\((.*?)\)'
-    match = re.search(pattern, record)
-    return {
-        'head': match.group(1),
-        'relation': match.group(2),
-        'tail': match.group(3)}
-
-
-def query_path(graph, relation, from_entity, to_entity, dataset_name, max_len):
-    query = graph.run(f'''
-            MATCH (from_entity{{name:"{from_entity}", dt_name:"{dataset_name}"}}), 
-            (to_entity{{name:"{to_entity}", dt_name:"{dataset_name}" }}),
-            p = (from_entity)-[*1..{max_len}]->(to_entity)
-            RETURN DISTINCT relationships(p) AS relations, nodes(p) as nodes;
-    ''')
+def query_path(driver, relation, from_entity, to_entity, dataset_name, max_len):
+    paths, _, _ = driver.execute_query(f'''
+                MATCH (from_entity{{name:"{from_entity}", dt_name:"{dataset_name}"}}), 
+                (to_entity{{name:"{to_entity}", dt_name:"{dataset_name}" }}),
+                p = (from_entity)-[*1..{max_len}]->(to_entity)
+                RETURN DISTINCT relationships(p) AS relations, nodes(p) as nodes;
+        ''')
     results = set()
-    paths = query.data()
     for j, path in enumerate(paths):
         path = path['relations']
         if len(path) == 1:
-            if extract_record_triplets(path[0])['relation'] == relation:
+            if path[0].type == relation:
                 continue
         rep_path = [relation]
         for record in path:
-            rep_path.append(extract_record_triplets(record)['relation'])
+            rep_path.append(record.type)
         results.add(' '.join(rep_path))
     return list(results)
 
 
-def inverse_relation(relation: str):
-    if relation.startswith('!'):
-        return relation.lstrip('!')
-    return '!' + relation
-
-
-def extract_paths(graph:Graph, df:pd.DataFrame, dataset_name, max_len):
+def extract_paths(driver, df:pd.DataFrame, dataset_name, max_len):
     patterns = []
     for i, row in tqdm(df.iterrows(), total=len(df)):
         # head -> tail direction
-        head, relation, tail = row['head'], row['relation'], row['tail']
-        head2tail_paths = query_path(graph=graph,
+        head, relation, inv_relation, tail = row['head'], row['relation'], row['inv_relation'], row['tail']
+        head2tail_paths = query_path(driver=driver,
                                      relation=relation,
                                      from_entity=head,
                                      to_entity=tail,
                                      dataset_name=dataset_name,
                                      max_len=max_len)
-        tail2head_paths = query_path(graph=graph,
-                                     relation=inverse_relation(relation),
+        tail2head_paths = query_path(driver=driver,
+                                     relation=inv_relation,
                                      from_entity=tail,
                                      to_entity=head,
                                      dataset_name=dataset_name,
@@ -70,30 +52,30 @@ def extract_paths(graph:Graph, df:pd.DataFrame, dataset_name, max_len):
 
 
 def count_relation(df:pd.DataFrame):
-    rel_count = Counter(df['relation'].tolist())
-    for key in list(rel_count.keys()):
-        rel_count['!' + key] = rel_count[key]
+    rel_count = Counter(df['relation'].tolist() + df['inv_relation'].tolist())
     return rel_count
 
 
 if __name__ == '__main__':
-    graph = Graph('bolt://localhost:7687')
+    # Note: Neo4j is more than 10 times faster then Py2neo
+    URI = "neo4j://localhost:7687"
+    global_driver = GraphDatabase.driver(URI)
+
     parser = argparse.ArgumentParser()
     parser.add_argument("--dataset", help="Name of dataset", required=True)
-    parser.add_argument("--max_len", help="Max length", required=True)
+    parser.add_argument("--max_len", help="Max length", required=True, type=int)
     args = parser.parse_args()
     if args.dataset not in ['WN18RR', 'FB15k-237']:
         raise ValueError('Wrong dataset name!!!')
     dataset = args.dataset
-    ent_dct = load_dict(f'{dataset}/entities.dict')
-    rel_dct = load_dict(f'{dataset}/relations.dict')
     train_data = load_data(f'{dataset}/train.txt')
     relation_count = count_relation(train_data)
 
-    patterns = extract_paths(graph, train_data, dataset, args.max_len)
+    patterns = extract_paths(global_driver, train_data, dataset, args.max_len)
 
     with open(f'{dataset}/patterns_mxl_{args.max_len}.txt', 'w') as f:
         for i, pat in enumerate(patterns):
             f.write(f'{pat[0]} {pat[1]} {round(pat[1] / relation_count[pat[0].split()[0]], 3)}\n')
+
 
 
