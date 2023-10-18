f7889969a8c86c9bb4bfb16a5f479093be9c42ca
Trung Le <le.hg.trung@gmail.com>
2023-10-18 17:11:50 -0600

    subgraph mining into a single folder, remove unused python scripts

--- mining/subgraph_mining_neo4j.py
+++ mining/subgraph_mining_neo4j.py
@@ -1,22 +1,13 @@
 import argparse
-import ast
 import os
-import time
 from datetime import datetime
 
 from neo4j import GraphDatabase
-import re
-import pandas as pd
-from tqdm import tqdm
 from reasoning_utils import *
 
 
-def extract_subgraph_as_atoms(driver, query):
-    # start_time = time.time()
+def extract_subgraph_as_triplets(driver, query):
     records, _, _ = driver.execute_query(query)
-    # end_time = time.time()
-    # elapsed_time = end_time - start_time
-    # print("Elapsed Time:", elapsed_time, "seconds")
     triplets = []
     for j, record in enumerate(records):
         # Each record is a path from center to a node
@@ -26,7 +17,7 @@ def extract_subgraph_as_atoms(driver, query):
             head = path.nodes[0]['name']
             tail = path.nodes[1]['name']
             relation = path.type
-            triplets.append(f'{relation}({head},{tail}).')
+            triplets.append(f'{head}\t{relation}\t{tail}')
     return set(triplets)
 
 
@@ -46,43 +37,38 @@ def extract_subgraphs(driver,
                       hops,
                       dataset,
                       out_path,
-                      start_at):
-    # query_template = """
-    #     MATCH path = (center:Entity {{name: '{name}', dt_name: '{dataset}'}})-[*..2]-(k:Entity)
-    #     RETURN relationships(path) as relation, nodes(path) as source_dest_nodes
-    # """
+                      start_at,
+                      end_at):
     query_template = """
             MATCH (center:Entity {{name: '{name}', dt_name: '{dataset}'}}),
             path = (center)-[*..{hops}]-(k:Entity)
             RETURN relationships(path) as relations, nodes(path) as nodes
         """
     for i, row in df.iterrows():
-        if i < start_at:
+        if i < start_at or i >= end_at:
             continue
         if i % 100 == 0:
             print(f'At step: {i}, current time: {datetime.now().strftime("%Y-%m-%d %H:%M")}')
         head, relation, inv_relation, tail = row['head'], row['relation'], \
                                              row['inv_relation'], row['tail']
-        expected = f'{relation}({head},{tail}).'
-        inv_expected = f'{inv_relation}({tail},{head}).'
+        expected = f'{head}\t{relation}\t{tail}'
+        inv_expected = f'{head}\t{inv_relation}\t{tail}'
 
         query = query_template.format(name=row['head'],
                                       hops=hops,
                                       dataset=dataset)
-        atoms = extract_subgraph_as_atoms(driver, query)
+        atoms = extract_subgraph_as_triplets(driver, query)
         atoms.discard(expected)
 
         query = query_template.format(name=row['tail'],
                                       hops=hops,
                                       dataset=dataset)
-        inv_atoms = extract_subgraph_as_atoms(driver, query)
+        inv_atoms = extract_subgraph_as_triplets(driver, query)
         inv_atoms.discard(inv_expected)
 
-        with open(out_path.format(idx=f'{i}.head'), 'w') as f:
-            f.write('%' + expected + '\n')
+        with open(out_path.format(idx=f'{i}.fwd'), 'w') as f:
             f.writelines([e + '\n' for e in atoms])
-        with open(out_path.format(idx=f'{i}.tail'), 'w') as f:
-            f.write('%' + inv_expected + '\n')
+        with open(out_path.format(idx=f'{i}.bwd'), 'w') as f:
             f.writelines([e + '\n' for e in inv_atoms])
 
 
@@ -94,8 +80,9 @@ if __name__ == '__main__':
     parser.add_argument("--dataset", help="Name of dataset", required=True)
     parser.add_argument("--source", help="Train or test", required=True)
     parser.add_argument("--part", help="Part number", type=int, required=True)
-    parser.add_argument("--hops", help="Number of hops", type=int, required=True)
-    parser.add_argument("--start_at", help="Start at", type=int, required=False, default=0)
+    parser.add_argument("--hops", help="Number of hops", type=int, required=False, default=3)
+    parser.add_argument("--start_at", help="Start at", type=int, required=True)
+    parser.add_argument("--end_at", help="Start at", type=int, required=True)
     args = parser.parse_args()
 
     if args.dataset not in ['WN18RR', 'FB15k-237']:
@@ -105,20 +92,30 @@ if __name__ == '__main__':
     hops = args.hops
     source = args.source
     start_at = args.start_at
+    end_at = args.end_at
+
+    print(f'Run on {dataset}, start at {start_at}, end at {end_at}')
 
     if source == 'test':
-        out_dir = f'WN18RR_{source}_{hops}hops_neo4j'
+        out_dir = f'{dataset}_{source}_{hops}hops_neo4j'
         if not os.path.exists(out_dir):
             os.makedirs(out_dir)
         out_path = out_dir + '/{idx}.txt'
         df = load_data_raw(f'../{dataset}/{source}.txt')
     else:
-        df = load_data_raw(f'../WN18RR/splits/part_{part}.txt')
-        out_dir = f'WN18RR_train_{hops}hops_neo4j/part={part}'
-        if not os.path.exists(out_dir):
-            os.makedirs(out_dir)
-        out_path = out_dir + '/{idx}.txt'
-    extract_subgraphs(global_driver, df, hops, 'WN18RR', out_path, start_at)
+        if part < 0:
+            df = load_data_raw(f'../{dataset}/train.txt')
+            out_dir = f'{dataset}_train_{hops}hops_neo4j_full'
+            if not os.path.exists(out_dir):
+                os.makedirs(out_dir)
+            out_path = out_dir + '/{idx}.txt'
+        else:
+            df = load_data_raw(f'../{dataset}/splits/part_{part}.txt')
+            out_dir = f'{dataset}_train_{hops}hops_neo4j/part={part}'
+            if not os.path.exists(out_dir):
+                os.makedirs(out_dir)
+            out_path = out_dir + '/{idx}.txt'
+    extract_subgraphs(global_driver, df, hops, dataset, out_path, start_at, end_at)
     global_driver.close()
 
     # compare_with_py2neo('WN18RR_train_2hops_py2neo/part=0/{i}.train.txt',
