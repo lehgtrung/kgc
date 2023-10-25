2e15dde3848b86346abad82698d8d276222940e0
Trung Le <le.hg.trung@gmail.com>
2023-10-25 16:44:43 -0600

    add pattern for fb15k

--- mining/matrix_reasoning.py
+++ mining/matrix_reasoning.py
@@ -53,8 +53,8 @@ def encode_rules(rule_path, max_rank=10):
     rules = {}
     for line in raw_rule_list:
         line = line.split()
-        if float(line[-1]) < 0.1:
-            continue
+        # if float(line[-1]) < 0.1:
+        #     continue
         rule_head = normalize_relation(line[0])
         rule_conf = float(line[-1])
         rule_body = [normalize_relation(e) for e in line[1:-2]]
